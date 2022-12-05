038c628764a97dd61b0191147554c7c7af9adf17
startdusk <522383345@qq.com>
2022-12-05 21:15:29 +0800

    fix🐛:  fixed

--- shared/filestat/filestat_linux.go
+++ shared/filestat/filestat_linux.go
@@ -9,6 +9,24 @@ import (
 	"time"
 )
 
+// type Stat_t struct {
+// 	Dev       uint64
+// 	Ino       uint64
+// 	Nlink     uint64
+// 	Mode      uint32
+// 	Uid       uint32
+// 	Gid       uint32
+// 	X__pad0   int32
+// 	Rdev      uint64
+// 	Size      int64
+// 	Blksize   int64
+// 	Blocks    int64
+// 	Atim      Timespec
+// 	Mtim      Timespec
+// 	Ctim      Timespec
+// 	X__unused [3]int64
+// }
+
 // FileStat indicates the file status of the specified system.
 type FileStat struct {
 	CreateTime int64
@@ -33,13 +51,12 @@ func Stat(path string) (FileStat, error) {
 	if !ok {
 		return filestat, fmt.Errorf("invalid system stat")
 	}
-	filestat.Dev = stat.Dev
+	filestat.Dev = int32(stat.Dev)
 	filestat.INO = stat.Ino
-	filestat.Mode = stat.Mode
+	filestat.Mode = uint16(stat.Mode)
 	filestat.UID = stat.Uid
 	filestat.GID = stat.Gid
 	filestat.Size = stat.Size
-	filestat.Flags = stat.Flags
 	filestat.CreateTime = time.Unix(stat.Ctim.Sec, stat.Ctim.Nsec).Unix()
 	filestat.ModifyTime = time.Unix(stat.Mtim.Sec, stat.Mtim.Nsec).Unix()
 	return filestat, nil
